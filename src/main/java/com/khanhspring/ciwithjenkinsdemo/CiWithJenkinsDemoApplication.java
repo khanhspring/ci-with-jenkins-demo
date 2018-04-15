65d04adef74656c249b16baff05162bed9d25cde
Tran Xuan Khanh <xuankhanhyb@gmail.com>
2018-04-15 22:03:47 +0700

    update main class

--- src/main/java/com/khanhspring/ciwithjenkinsdemo/CiWithJenkinsDemoApplication.java
+++ src/main/java/com/khanhspring/ciwithjenkinsdemo/CiWithJenkinsDemoApplication.java
@@ -1,9 +1,11 @@
 package com.khanhspring.ciwithjenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class CiWithJenkinsDemoApplication {
 
 	public static void main(String[] args) {
