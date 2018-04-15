cf44e36ee116bcf1e2c1826535cfc1c32517e5b2
Tran Xuan Khanh <xuankhanhyb@gmail.com>
2018-04-15 22:23:27 +0700

    add row to main class

--- src/main/java/com/khanhspring/ciwithjenkinsdemo/CiWithJenkinsDemoApplication.java
+++ src/main/java/com/khanhspring/ciwithjenkinsdemo/CiWithJenkinsDemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableAutoConfiguration
 public class CiWithJenkinsDemoApplication {
 
+  
 	public static void main(String[] args) {
 		SpringApplication.run(CiWithJenkinsDemoApplication.class, args);
 	}
