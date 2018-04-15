e09f23416427e33e4b85550710ae41e9e9fb489e
Tran Xuan Khanh <xuankhanhyb@gmail.com>
2018-04-15 19:29:03 +0700

    initial project

---
+++ src/main/java/com/khanhspring/ciwithjenkinsdemo/CiWithJenkinsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.khanhspring.ciwithjenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiWithJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiWithJenkinsDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/khanhspring/ciwithjenkinsdemo/CiWithJenkinsDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.khanhspring.ciwithjenkinsdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CiWithJenkinsDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
